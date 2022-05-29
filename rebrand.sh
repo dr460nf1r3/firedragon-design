adb38b5897b530b96952aadeececa521822fe60b
dr460nf1r3 <njcrypted@protonmail.com>
2022-05-29 19:27:34 +0200

    Improve patch rebranding

--- rebrand.sh
+++ rebrand.sh
@@ -3,5 +3,11 @@ rebrand() {
     find ./* -type f -exec sed -i "s/$1/$2/g" {} +
 }
 
+rebrand "\/io\/gitlab\/" "\/org\/garudalinux\/"
+rebrand "io.gitlab." "org.garudalinux."
 rebrand LibreWolf FireDragon
+rebrand Librewolf FireDragon
+rebrand librewolf firedragon
+rebrand "fredragon\.net" "librewolf.net"
+rebrand "#why-is-firedragon-forcing-light-theme" "#why-is-librewolf-forcing-light-theme"
 rebrand kmozillahelper kfiredragonhelper
